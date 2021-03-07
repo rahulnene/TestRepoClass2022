064f54161ca78f586518a56fea4f9941d3660653
Rahul nene <rahul.nene@live.com>
2021-03-08 01:07:19 +0530

    init

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import nistrng
 import math
 from fractions import Fraction
-from
+from gamma_functions import *
+from sp800_22_monobit_test import monobit_test
 
 bit_file = open("petra.txt", "r")
 
@@ -17,18 +18,6 @@ def count_ones_zeroes(bitstream):
     return zeroes, ones
 
 
-def monobit_test(bits):
-    n = len(bits)
-
-    zeroes, ones = count_ones_zeroes(bits)
-    s = abs(ones - zeroes)
-    print("  Ones count   = %d" % ones)
-    print("  Zeroes count = %d" % zeroes)
-
-    p = math.erfc(float(s) / (math.sqrt(float(n)) * math.sqrt(2.0)))
-
-    success = (p >= 0.01)
-    return success, p
 
 
 def frequency_within_block_test(bits):
